7a70956b0c99728e079d8dffd7df155346369480
francisco suarez <franns008@gmail.com>
2024-05-06 21:51:35 -0300

    cambios oficiales

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,9 @@ from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CreateNewTask, CreateNewProject
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 
@@ -16,23 +19,48 @@ def index(request):
 
 
 def menuPrincipal(request):
-    title = "Menu Principal"
     return render(request, 'menuPrincipal.html')
 
 def  miPerfil(request):
     previous_page = request.META.get('HTTP_REFERER')
-    usuario = request.user  # Obtenemos el usuario autenticado
+    #usuario = request.user  # Obtenemos el usuario autenticado
+    usuario = User.objects.all()[3]
+  
     if request.method == 'POST':
-        # Actualizamos los campos del usuario con los datos del formulario
-        usuario.username = request.POST['username']
-        usuario.email = request.POST['email']
-        usuario.first_name = request.POST['first_name']
-        usuario.last_name = request.POST['last_name']
-        usuario.save()  # Guardamos los cambios en la base de datos
-        messages.success(request, 'Los cambios se han guardado correctamente.')
+        if 'contraseñaActual' in request.POST:
+            # El formulario se envió desde el modal de cambio de contraseña
+            contraseña_actual = request.POST.get('contraseñaActual')
+            nueva_contraseña = request.POST.get('contraseñaNueva')
+            repetir_nueva_contraseña = request.POST.get('repetirContraseñaNueva')
+            
+            # Verificar si la contraseña actual coincide con la del usuario
+            if usuario.check_password(contraseña_actual):
+                try:
+                    validate_password(nueva_contraseña, user=usuario)
+                except ValidationError as error:
+                    messages.error(request, error.messages[0])
+                else:  # Solo si la validación de la contraseña pasa sin excepciones
+                    if nueva_contraseña == repetir_nueva_contraseña:
+                        # Actualizar la contraseña del usuario
+                        usuario.set_password(nueva_contraseña)
+                        usuario.save()
+                        messages.success(request, 'La contraseña se ha actualizado correctamente.')
+                    else:
+                        messages.error(request, 'Las nuevas contraseñas no coinciden.')
+            else:
+                messages.error(request, 'La contraseña actual es incorrecta.')
+        else:
+            # El formulario se envió desde el botón "Guardar cambios" fuera del modal
+            usuario.username = request.POST['username']
+            usuario.email = request.POST['email']
+            usuario.first_name = request.POST['first_name']
+            usuario.last_name = request.POST['last_name']
+            usuario.save()  # Guardar los cambios en la base de datos
+            messages.success(request, 'Los cambios se han guardado correctamente.')
     return render(request, 'miPerfil.html', {
         'previous_page': previous_page, 
-        'usuario': usuario})
+        'usuario': usuario}
+    )
 
 def about(request):
     username = 'fazt'
