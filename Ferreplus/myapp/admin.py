5f7be436544c47feb12789366c1a19718e4e6ec8
franns008 <127999669+franns008@users.noreply.github.com>
2024-07-02 10:26:22 -0300

    Delete Ferreplus directory

--- Ferreplus/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Sucursal, intercambios, Product, Rating
-
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(Sucursal)
-admin.site.register(intercambios)
-admin.site.register(Product)
-admin.site.register(Rating)
